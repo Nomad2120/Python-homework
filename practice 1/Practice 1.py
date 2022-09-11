8d265c9b4fe1029a0d0bbd55dd856915a733ffdd
Nomad2120 <96611432+Nomad2120@users.noreply.github.com>
2022-09-11 20:02:29 +0600

    Delete practice 1 directory

--- practice 1/Practice 1.py
+++
@@ -1,6 +0,0 @@
-name=input('Your last name, first name?')
-age=input('How old are you')
-location=input('Where do you live?')
-print('Your name',name)
-print('your age',age)
-print('you live in',location)
